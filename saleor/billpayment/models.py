4990d7aeee5829ecf808b7774768db91b24e74f6
Kiburu Alex <alexkiburu@gmail.com>
2018-04-28 08:04:58 +0300

    added fields in the bill, billtypes, deposit and billpayments models

--- saleor/billpayment/models.py
+++ saleor/billpayment/models.py
@@ -1,15 +1,39 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from decimal import Decimal
 from django.utils.translation import pgettext_lazy
+from django.core.validators import MinValueValidator, RegexValidator
 from django.utils.timezone import now
+from saleor.bill.models import Bill
+from saleor.customer.models import Customer
+from saleor.room.models import Room
+from saleor.paymentoptions.models import PaymentOptions
 
 
 class BillPayment(models.Model):
+    ''' invoice_number for generating invoices of that bill '''
+    invoice_number = models.CharField(
+        pgettext_lazy('BillPayment field', 'invoice_number'), unique=True, null=True, max_length=36)
     name = models.CharField(
         pgettext_lazy('BillPayment field', 'name'), unique=True, max_length=128)
     description = models.TextField(
         verbose_name=pgettext_lazy('BillPayment field', 'description'), blank=True, null=True)
+    bill = models.ForeignKey(
+        Bill, blank=True, null=True, related_name='billpayment_types',
+        verbose_name=pgettext_lazy('BillPayment field', 'customer'), on_delete=models.SET_NULL)
+    customer = models.ForeignKey(
+        Customer, blank=True, null=True, related_name='billpayment_customers',
+        verbose_name=pgettext_lazy('BillPayment field', 'customer'), on_delete=models.SET_NULL)
+    room = models.ForeignKey(
+        Room, blank=True, null=True, related_name='billpayment_rooms',
+        verbose_name=pgettext_lazy('BillPayment field', 'room'), on_delete=models.SET_NULL)
+    payment_option = models.ForeignKey(
+        PaymentOptions, blank=True, null=True, related_name='billpayment_rooms',
+        verbose_name=pgettext_lazy('BillPayment field', 'room'), on_delete=models.SET_NULL)
+    transaction_number = models.CharField(
+        pgettext_lazy('BillPayment field', 'transaction_number'), null=True, max_length=128)
 
     updated_at = models.DateTimeField(
         pgettext_lazy('BillPayment field', 'updated at'), auto_now=True, null=True)
--- saleor/deposit/models.py
+++ saleor/deposit/models.py
@@ -1,15 +1,41 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from decimal import Decimal
 from django.utils.translation import pgettext_lazy
 from django.utils.timezone import now
+from django_prices.models import PriceField
+from django.core.validators import MinValueValidator, RegexValidator
+from saleor.billtypes.models import BillTypes
+from saleor.customer.models import Customer
+from saleor.room.models import Room
+
 
 
 class Deposit(models.Model):
+    ''' invoice_number for generating invoices of that bill '''
+    invoice_number = models.CharField(
+        pgettext_lazy('Deposit field', 'invoice_number'), unique=True, null=True, max_length=36)
     name = models.CharField(
         pgettext_lazy('Deposit field', 'name'), unique=True, max_length=128)
     description = models.TextField(
         verbose_name=pgettext_lazy('Deposit field', 'description'), blank=True, null=True)
+    customer = models.ForeignKey(
+        Customer, blank=True, null=True, related_name='deposit_customers',
+        verbose_name=pgettext_lazy('Deposit field', 'customer'), on_delete=models.SET_NULL)
+    room = models.ForeignKey(
+        Room, blank=True, null=True, related_name='deposit_rooms',
+        verbose_name=pgettext_lazy('Deposit field', 'room'), on_delete=models.SET_NULL)
+    amount = PriceField(
+        pgettext_lazy('Deposit field', 'amount of the Deposit'),
+        currency=settings.DEFAULT_CURRENCY, max_digits=12,
+        validators=[MinValueValidator(0)], default=Decimal(0), decimal_places=2)
+    month = models.DateTimeField(
+        pgettext_lazy('Deposit field', 'month billed'),
+        default=now)
+    status = models.CharField(
+        pgettext_lazy('Deposit field', 'status'), default='pending', null=True, max_length=128)
 
     updated_at = models.DateTimeField(
         pgettext_lazy('Deposit field', 'updated at'), auto_now=True, null=True)
